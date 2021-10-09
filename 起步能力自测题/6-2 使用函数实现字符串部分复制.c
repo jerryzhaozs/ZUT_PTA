bba515f7534b8ae7211416bce2b8f2cd98f85ab0
赵孜硕 <jerryzhao2002@126.com>
2021-10-09 14:26:08 +0800

    55

--- 起步能力自测题/6-2 使用函数实现字符串部分复制.c
+++
@@ -1,9 +0,0 @@
-void strmcpy( char *t, int m, char *s ) {
-	int i=m-1, j=0;
-	while ( t[i] != '\0' ){//如果字符串t没有结束就继续复制
-		s[j] = t[i];
-        j++;
-        i++;//两下标右移
-    }
-	s[j]='\0';//字符串尾结束符
-}
